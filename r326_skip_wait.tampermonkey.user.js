8dc2743176fb96ab27333f072401f282ce62350c
Ryo Ikeda <4lex-gs@users.noreply.github.com>
2023-04-13 12:33:26 +0900

    Update r326_skip_wait.tampermonkey.user.js

--- r326_skip_wait.tampermonkey.user.js
+++ r326_skip_wait.tampermonkey.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.r326.com/b/complete.aspx?id=*
+// @match        https://www.r326.com/b/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=r326.com
 // @grant        none
 // @copyright  2022+, Ryo Ikeda
@@ -13,12 +13,31 @@
 
 setTimeout(() => {
     console.log('Skip waiting called');
-    var a = document.getElementById("form1");
-    var hiddens = document.querySelectorAll('.aspNetHidden');
-    hiddens.forEach((e) => {
-       e.innerHTML = '';
-    });
-    a.setAttribute("method", "POST");
-    a.setAttribute("action", "main.aspx?" + window.GetIdOrG());
-    a.submit()
+
+    /** 予約完了画面の場合の処理 */
+    if (location.pathname === "/b/complete.aspx") {
+        var a = document.getElementById("form1");
+        var hiddens = document.querySelectorAll('.aspNetHidden');
+        hiddens.forEach((e) => {
+            e.innerHTML = '';
+        });
+        a.setAttribute("method", "POST");
+        a.setAttribute("action", "main.aspx?" + window.GetIdOrG());
+        a.submit()
+    }
+
+}, 500);
+
+setTimeout(() => {
+    console.log('Remove Ads called', location.pathname);
+
+    /** TODO: 広告消去の処理実装予定 */
+    if (location.pathname === "/b/main.aspx") {
+        const overlay = document.getElementById('overlay');
+        if (overlay) {
+            overlay.remove();
+            const modal = document.querySelector('div.modal-window');
+            modal?.remove();
+        }
+    }
 }, 500);
